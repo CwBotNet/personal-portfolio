c4ced160b44a44c4ca06c5cc1352c64806693b6e
Raj sahani <Rajsahaniofficial19@gmail.com>
2024-02-16 22:54:24 +0530

    added about section ui sklition

--- components/client/AboutSection.tsx
+++ components/client/AboutSection.tsx
@@ -1,9 +1,56 @@
 import React from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
 
 const AboutSection = () => {
     return (
-        <div id="about" className='bg-yellow-300 h-[400px] md:h-screen'>
-            about Section
+        <div id="about" className='md:h-screen'>
+            <Card>
+                <CardHeader>
+                    <CardTitle className='text-center'>
+                        About me
+                    </CardTitle>
+                </CardHeader>
+
+                <CardContent className='flex md:flex-col flex-wrap gap-4'>
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>
+                                Education
+                            </CardTitle>
+                        </CardHeader>
+                        <CardContent>
+
+                        </CardContent>
+                    </Card>
+
+                    {/*  */}
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>
+                                Skills
+                            </CardTitle>
+                        </CardHeader>
+                        <CardContent>
+
+                        </CardContent>
+                    </Card>
+
+                    {/*  */}
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>
+                                certification
+                            </CardTitle>
+                        </CardHeader>
+                        <CardContent>
+
+                        </CardContent>
+                    </Card>
+                </CardContent>
+            </Card>
+
+            {/*  */}
+
         </div>
     )
 }
