af0dd37e3df473723cefe432077c1eacc18a5cae
Raj sahani <Rajsahaniofficial19@gmail.com>
2024-06-14 01:39:53 +0530

    cors bug fix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,47 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // cors
+  async headers() {
+    return [
+      {
+        // Routes this applies to
+        source: "/api/(.*)",
+        // Headers
+        headers: [
+          // Allow for specific domains to have access or * for all
+          {
+            key: "Access-Control-Allow-Origin",
+            value: process.env.ALLOWED_ORIGIN,
+            // DOES NOT WORK
+            // value: process.env.ALLOWED_ORIGIN,
+          },
+          // Allows for specific methods accepted
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          // Allows for specific headers accepted (These are a few standard ones)
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
+
   images: {
-    domains: [
-      "api.microlink.io",
-      "prod-files-secure.s3.us-west-2.amazonaws.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.microlink.io",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "prod-files-secure.s3.us-west-2.amazonaws.com",
+        pathname: "**",
+      },
     ],
   },
   reactStrictMode: true,
